c98860bd268963671e18d2e8f45105d10626cb51
DaanishShaikh <daanish222@gmail.com>
2024-02-09 21:43:47 +0530

    09-02-2024

--- Janmansh_and_Assignments.cpp
+++ Janmansh_and_Assignments.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int t;
+    int t;          //testcase
     cin>>t;
     int a[t];
     for(int i= 0; i<t ; i++){
-        cin>>a[i];
+        cin>>a[i];                    //taking inputs
     }
     for(int k= 0; k<t ; k++){
         if(a[k]+3<=10){
