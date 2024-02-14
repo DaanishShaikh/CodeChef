a317fb2cbb2084bb08ad00a2897e74599f356450
DaanishShaikh <daanish222@gmail.com>
2024-02-14 20:09:22 +0530

    14-02-2024

--- exams.cpp
+++ exams.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int T;
+    cin>>T;
+    for(int i=0 ; i<T ; i++){
+    int X,Y,Z;
+    cin>>X>>Y>>Z;
+    if((X*Y)/2<Z){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+
+    }
+    return 0;
 }
