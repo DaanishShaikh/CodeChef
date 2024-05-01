85da1ce2d6a949178e4b782579e7cf11e9182033
DaanishShaikh <daanish222@gmail.com>
2024-05-01 21:46:32 +0530

    01-05-2024

---
+++ change_please.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using i64=long long;
+
+void solve(){
+    
+    int n;
+    cin >> n;
+    int ans=100-n;
+    cout<<(ans/10)*10<<endl;
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt;
+    cin >> tt;
+
+    while(tt--){
+        solve();
+    }
+
+    return 0;
+}
