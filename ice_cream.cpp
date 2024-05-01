192646a82cf3c1b1584103c08ee6cf8ca2c323bd
DaanishShaikh <daanish222@gmail.com>
2024-05-01 21:47:22 +0530

    01-05-2024

---
+++ ice_cream.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using i64=long long;
+
+void solve(){
+    
+    int x,y;
+    cin >> x >> y;
+    if(y>=2*x){
+        cout<<"YES"<<endl;
+    } else {
+        cout<<"NO"<<endl;
+    }
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+        solve();
+
+
+    return 0;
+}
